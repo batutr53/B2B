8e1438724a3a1c54ee4b35dbc6163a21ffd4e8cd
Batuhan Türk <batuay34@outlook.com>
2022-09-09 15:18:52 +0300

    Add project files.

---
+++ B2B.Entities/Concrete/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Entities.Concrete
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Model ModelId { get; set; }
+    }
+}
---
+++ B2B.Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Entities.Concrete
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public  ProductDetail ProductDetailId { get; set; }
+        public Brand BrandId { get; set; }
+    }
+}
